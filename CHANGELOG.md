c47236c60bb04c6be13d9a19360eaa8b7bb6c4eb
Joseph Scott <joseph@josephscott.org>
2023-04-06 23:02:23 -0600

    Add support for custom request headers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## ????
 - Lowercase all response header names
 - Set types for function args
+- Add support for custom request headers
 
 ## 0.0.1 - 2023-04-06
 - Basic minimal working version
--- tegstop.php
+++ tegstop.php
@@ -12,9 +12,21 @@ function http_parse_headers( array $headers ) {
     return $parsed;
 }
 
-function http_get( string $url ) {
+function http_get( string $url, array $headers = [] ) {
 	$response = [];
-	$body = file_get_contents( $url );
+
+	$options = [
+		'http' => [
+			'method' => 'GET',
+			'header' => ''
+		]
+	];
+	foreach ( $headers as $header_name => $header_value ) {
+		$options['http']['header'] .= "$header_name: $header_value\r\n";
+	}
+
+    $context = stream_context_create( $options );
+	$body = file_get_contents( $url, false, $context );
 
 	$response['error'] = false;
 	$response['headers'] = http_parse_headers( $http_response_header );
@@ -28,16 +40,19 @@ function http_get( string $url ) {
 	return $response;
 }
 
-function http_post( string $url, array $data ) {
+function http_post( string $url, array $headers = [], array $data = [] ) {
 	$response = [];
 
 	$options = [
 		'http' => [
 			'method' => 'POST',
-			'header' => 'Content-Type: application/x-www-form-urlencoded',
+			'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
 			'content' => http_build_query( $data ),
 		],
 	];
+	foreach ( $headers as $header_name => $header_value ) {
+		$options['http']['header'] .= "$header_name: $header_value\r\n";
+	}
 
     $context = stream_context_create( $options );
 	$body = file_get_contents( $url, false, $context );
