68760409e7baea461102152c92999440da27314c
Joseph Scott <joseph@josephscott.org>
2023-04-06 23:24:20 -0600

    Move everything to a single class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 - Lowercase all response header names
 - Set types for function args
 - Add support for custom request headers
+- Move everything to a single class
 
 ## 0.0.1 - 2023-04-06
 - Basic minimal working version
--- tegstop.php
+++ tegstop.php
@@ -1,70 +1,78 @@
 <?php
-function http_parse_headers( array $headers ) {
-	$parsed = [];
+class Tegstop {
+	public function __construct() { }
 
-	foreach ( $headers as $header ) {
-		$parts = explode( ':', $header, 2 );
-		if ( count( $parts ) === 2 ) {
-			$parsed[ strtolower( trim( $parts[0] ) ) ] = trim( $parts[1] );
+	public function get( string $url, array $headers = [] ) {
+		$response = [];
+
+		$options = [
+			'http' => [
+				'method' => 'GET',
+				'header' => ''
+			]
+		];
+		foreach ( $headers as $header_name => $header_value ) {
+			$options['http']['header'] .= "$header_name: $header_value\r\n";
 		}
-	}
 
-    return $parsed;
-}
+		$context = stream_context_create( $options );
+		$body = file_get_contents( $url, false, $context );
 
-function http_get( string $url, array $headers = [] ) {
-	$response = [];
+		$response['error'] = false;
+		$response['headers'] = self::parse_headers( $http_response_header );
+		$response['body'] = $body;
 
-	$options = [
-		'http' => [
-			'method' => 'GET',
-			'header' => ''
-		]
-	];
-	foreach ( $headers as $header_name => $header_value ) {
-		$options['http']['header'] .= "$header_name: $header_value\r\n";
-	}
+		if ( $body === false ) {
+			$response['error'] = true;
+			return $response;
+		}
 
-    $context = stream_context_create( $options );
-	$body = file_get_contents( $url, false, $context );
-
-	$response['error'] = false;
-	$response['headers'] = http_parse_headers( $http_response_header );
-	$response['body'] = $body;
-
-	if ( $body === false ) {
-		$response['error'] = true;
 		return $response;
 	}
 
-	return $response;
-}
+	public function post( string $url, array $headers = [], array $data = [] ) {
+		$response = [];
 
-function http_post( string $url, array $headers = [], array $data = [] ) {
-	$response = [];
+		$options = [
+			'http' => [
+				'method' => 'POST',
+				'header' => '',
+				'content' => http_build_query( $data ),
+			],
+		];
+		$options['http']['header'] .= 'Content-Type: '
+			. "application/x-www-form-urlencoded\r\n";
+		foreach ( $headers as $header_name => $header_value ) {
+			$options['http']['header'] .= "$header_name: $header_value\r\n";
+		}
 
-	$options = [
-		'http' => [
-			'method' => 'POST',
-			'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
-			'content' => http_build_query( $data ),
-		],
-	];
-	foreach ( $headers as $header_name => $header_value ) {
-		$options['http']['header'] .= "$header_name: $header_value\r\n";
-	}
+		$context = stream_context_create( $options );
+		$body = file_get_contents( $url, false, $context );
 
-    $context = stream_context_create( $options );
-	$body = file_get_contents( $url, false, $context );
+		$response['error'] = false;
+		$response['headers'] = self::parse_headers( $http_response_header );
+		$response['body'] = $body;
 
-	$response['error'] = false;
-	$response['headers'] = http_parse_headers( $http_response_header );
-	$response['body'] = $body;
+		if ( $body === false ) {
+			$response['error'] = true;
+			return $response;
+		}
 
-	if ( $body === false ) {
-		$response['error'] = true;
 		return $response;
 	}
 
-	return $response;
+	/* Private */
+
+	private function parse_headers( array $headers ) {
+		$parsed = [];
+
+		foreach ( $headers as $header ) {
+			$parts = explode( ':', $header, 2 );
+			if ( count( $parts ) === 2 ) {
+				$parsed[ strtolower( trim( $parts[0] ) ) ] = trim( $parts[1] );
+			}
+		}
+
+		return $parsed;
+	}
 }
