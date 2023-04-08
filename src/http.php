22d7d883bcf3e62ab27683b89db5a41553d4b614
Joseph Scott <joseph@josephscott.org>
2023-04-07 22:50:44 -0600

    Enable strict_types

--- src/http.php
+++ src/http.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 class HTTP {
 	public $default_headers = [
 		'user_agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 php-http/0.0.2',
