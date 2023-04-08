674a5d81af25e7387e3e8adc66aee197f29bfa44
Joseph Scott <joseph@josephscott.org>
2023-04-08 00:07:44 -0600

    Expand basic get test

--- tests/GetTest.php
+++ tests/GetTest.php
@@ -4,6 +4,8 @@ declare( strict_types = 1 );
 test( 'get', function() {
 	$http = new HTTP();
 
-	$response = $http->get( url: 'https://httpbin.org/broken/path' );
-	expect( $response['error'] )->toBe( true );
+	$response = $http->get( url: 'https://httpbin.org/get' );
+	expect( $response['error'] )->toBe( false );
+	expect( $response['headers']['response_code'] )->toBe( 200 );
+	expect( $response['headers']['content-type'] )->toBe( 'application/json' );
 } );
