15e59053818d6876d85bc2c4b1cb18332206bfdf
Joseph Scott <joseph@josephscott.org>
2023-04-07 23:46:30 -0600

    Remove left over error_log call

--- tests/GetTest.php
+++ tests/GetTest.php
@@ -5,6 +5,5 @@ test( 'get', function() {
 	$http = new HTTP();
 
 	$response = $http->get( url: 'https://httpbin.org/broken/path' );
-	error_log( var_export( $response, true ) );
 	expect( $response['error'] )->toBe( true );
 } );
