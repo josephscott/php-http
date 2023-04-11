4270ded33e3bb70db06d92b9c33632f5a0a330f3
Joseph Scott <joseph@josephscott.org>
2023-04-11 13:55:59 -0600

    Match the method var with the real method used

--- tests/server/index.php
+++ tests/server/index.php
@@ -1,18 +1,25 @@
 <?php
+$method = $_GET['method'] ?? null;
+$method = strtolower( $method );
+if ( strtolower( $_SERVER['REQUEST_METHOD'] ) !== $method ) {
+	header( 'HTTP/1.0 405 Method Not Allowed' );
+	exit;
+}
+
 $sleep = $_GET['sleep'] ?? 0;
 sleep( $sleep );
 
 $out = [];
 foreach ( apache_request_headers() as $k => $v ) {
-	$out[$k] = $v;
+	$out['headers'][$k] = $v;
 }
 
 foreach ( $_GET as $k => $v ) {
-	$out['GET'][$k] = $v;
+	$out['get'][$k] = $v;
 }
 
 foreach ( $_POST as $k => $v ) {
-	$out['POST'][$k] = $v;
+	$out['post'][$k] = $v;
 }
 
 $json = json_encode( $out, JSON_PRETTY_PRINT );
